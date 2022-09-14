b55c0b09ef4d810449c7fb44aadf088b38a2e92b
Bian <2877434330@qq.com>
2022-09-14 17:13:43 +0800

    创建acapp端和web端

---
+++ backend/src/main/java/com/kop/springboot/controller/pk/IndexController.java
@@ -0,0 +1,9 @@
+package com.kop.springboot.controller.pk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pk/")
+public class IndexController {
+}
