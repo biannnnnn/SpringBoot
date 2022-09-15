a26a55769ff745a94c11cf8674b72c32f78f2a74
Bian <2877434330@qq.com>
2022-09-15 11:25:26 +0800

    实现前后端数据交互

--- backend/src/main/java/com/kop/springboot/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kop/springboot/controller/pk/BotInfoController.java
@@ -3,12 +3,18 @@ package com.kop.springboot.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
 
     @RequestMapping("getBotInfo/")
-    public String getBotInfo() {
-        return "jjj";
+    public Map<String, String> getBotInfo() {
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "biandixing");
+        bot1.put("rating", "1500");
+        return bot1;
     }
 }
